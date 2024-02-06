effa0f97e4883ecb8fd511bcf68b31733bd2dde2
ferupk <feru.pratama@ui.ac.id>
2024-02-06 18:31:48 +0700

    Set Product ID on creation

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
@@ -6,12 +6,15 @@ import org.springframework.stereotype.Repository;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.UUID;
 
 @Repository
 public class ProductRepository {
     private List<Product> productData = new ArrayList<>();
 
     public Product create(Product product) {
+        String productId = UUID.randomUUID().toString();
+        product.setProductId(productId);
         productData.add(product);
         return product;
     }
