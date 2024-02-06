30a8ab8a9e9f1954d9b40619f88285b0aa64b73a
ferupk <feru.pratama@ui.ac.id>
2024-02-06 22:08:15 +0700

    Add Product editing logic

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
@@ -19,6 +19,12 @@ public class ProductRepository {
         return product;
     }
 
+    public void edit(String targetId, Product editValues) {
+        Product product = findById(targetId);
+        product.setProductName(editValues.getProductName());
+        product.setProductQuantity(editValues.getProductQuantity());
+    }
+
     public Iterator<Product> findAll() {
         return productData.iterator();
     }
