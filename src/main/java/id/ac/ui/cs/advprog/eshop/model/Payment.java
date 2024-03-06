c626531b9ad73506433134e7e6e87e5ce93ad3a9
ferupk <feru.pratama@ui.ac.id>
2024-03-06 17:54:10 +0700

    [GREEN] Implement Payment model

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -1,25 +1,78 @@
 package id.ac.ui.cs.advprog.eshop.model;
 
-import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Arrays;
 import java.util.Map;
 
-@Builder
 @Getter
 public class Payment {
     String id;
     String method;
-    @Setter
     String status;
     Map<String, String> paymentData;
 
     public Payment(String id, String method, Map<String,String> paymentData) {
+        this.id = id;
+        this.paymentData = paymentData;
 
+        String[] methodList = {"Voucher Code", "Cash On Delivery"};
+        if (Arrays.stream(methodList).noneMatch(item -> (item.equals(method)))) {
+            throw new IllegalArgumentException();
+        } else {
+            this.method = method;
+        }
+
+        if (this.method.equals("Voucher Code")) {
+            this.status = isVoucherDataValid() ? "SUCCESS" : "REJECTED";
+        } else {
+            this.status = isCashOnDeliveryDataValid() ? "SUCCESS" : "REJECTED";
+        }
     }
 
-    public Payment(String id, String method, String status, Map<String,String> paymentData) {
+    public void setStatus(String status) {
+        String[] statusList = {"SUCCESS", "REJECTED"};
+        if (Arrays.stream(statusList).noneMatch(item -> (item.equals(status)))) {
+            throw new IllegalArgumentException();
+        } else {
+            this.status = status;
+        }
+    }
 
+    private boolean isVoucherDataValid() {
+        String voucherCode = this.paymentData.get("voucherCode");
+        
+        if (voucherCode.length() != 16) {
+            return false;
+        } else if (!voucherCode.substring(0,5).equals("ESHOP")) {
+            return false;
+        } else if (countDigitsInCode(voucherCode) != 8) {
+            return false;
+        }
+
+        return true;
+    }
+
+    private int countDigitsInCode(String voucherCode) {
+        int sum = 0;
+        for (int i = 0; i < voucherCode.length(); i++) {
+            if (Character.isDigit(voucherCode.charAt(i)))
+                sum++;
+        }
+        return sum;
+    }
+
+    private boolean isCashOnDeliveryDataValid() {
+        String address = this.paymentData.get("address");
+        String deliveryFee = this.paymentData.get("deliveryFee");
+
+        if (address == null || address.isEmpty()) {
+            return false;
+        } else if (deliveryFee == null || deliveryFee.isEmpty()) {
+            return false;
+        }
+
+        return true;
     }
 }
